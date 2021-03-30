a9ef6a7d991a775f5cd87a6dee235cff3f19d169
czm <chzhm159@163.com>
2021-03-30 22:31:47 +0800

    添加quartz依赖

--- core/src/main/java/org/idw/core/server/App.java
+++ core/src/main/java/org/idw/core/server/App.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class App {
     private static final Logger log = LoggerFactory.getLogger(App.class);
     public static void main(String[] args) {
-
+        
 
     }
     public static void start(){
